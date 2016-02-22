7d2bd2427c176bc69b82c7fba8cbea88dab47808
David Molinari <contact@davidmolinari.fr>
2016-02-22 17:10:23 +0100

    meh

---
+++ unePile/unePile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace unePile
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public class Pile
+        {
+            private int maxElt;
+            public int MaxElt { get; set; }
+
+            public List<string> listTab;
+        }
+
+        public Pile InitPile(int maxValeurs)
+        {
+            var unePile = new Pile();
+            return unePile;
+        }
+
+    }
+}
