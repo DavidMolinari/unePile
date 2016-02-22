c7fa304fa1fd856814539da95967c61c0a3c8b93
David Molinari <contact@davidmolinari.fr>
2016-02-22 17:15:17 +0100

    Ajout de PileVide / PilePleine

--- unePile/unePile/Program.cs
+++ unePile/unePile/Program.cs
@@ -22,11 +22,44 @@ namespace unePile
             public List<string> listTab;
         }
 
+
+        /// <summary>
+        /// Initialisation de la pile 
+        /// 
+        /// TODO PAS ENCORE COMPLETE
+        /// 
+        /// 
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="maxValeurs"></param>
+        /// <returns></returns>
         public Pile InitPile(int maxValeurs)
         {
             var unePile = new Pile();
             return unePile;
         }
+
+        /// <summary>
+        /// Retourne true si la liste ne contient aucun élément, donc si la pile est vide.
+        /// </summary>
+        /// <param name="unePile"></param>
+        /// <returns></returns>
+        public bool PileVide( Pile unePile)
+        {
+            return unePile.listTab.Count == 0 ? true : false;
+        }
+
+        /// <summary>
+        /// Retourne true si la liste a atteint son "sommet" et faux s'il y'a encore de la place.
+        /// </summary>
+        /// <param name="unePile"></param>
+        /// <returns></returns>
+        public bool PilePleine (Pile unePile)
+        {
+            return unePile.listTab.Count == unePile.MaxElt ? true : false;
+        }
+
 
     }
 }
